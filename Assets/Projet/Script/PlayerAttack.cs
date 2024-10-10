8f35f762651e8906df4df22f3bfe3debff187fdb
Louan Ourvouai <megakitraz3000@gmail.com>
2024-10-10 22:57:05 +0200

    Add the network to shoot a missile

--- Assets/Projet/Script/PlayerAttack.cs
+++ Assets/Projet/Script/PlayerAttack.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using System;
 
 public class PlayerAttack : NetworkBehaviour
 {
     [SerializeField] private GameObject m_missilePrefab;
-
     [SerializeField] private Transform m_shootPosition;
 
-    public GameObject m_missileGameObject;
+    [NonSerialized] public GameObject m_missileGameObject;
     private Missile m_missile;
 
     private void Start()
@@ -17,15 +17,42 @@ public class PlayerAttack : NetworkBehaviour
         m_missileGameObject = null;
     }
 
+    // This function is called on the client, but it sends a command to the server
     public void ShootMissile()
     {
-        
-        if(m_missileGameObject == null)
+        if (isLocalPlayer)  // Ensure only the local player can shoot
         {
-            m_missileGameObject = Instantiate(m_missilePrefab, m_shootPosition.position, m_shootPosition.rotation);
+            CmdShootMissile();
+        }
+    }
+
+    // This command is called on the server when a client requests to shoot a missile
+    [Command]
+    private void CmdShootMissile()
+    {
+        if (m_missileGameObject == null)
+        {
+            // Instantiate the missile at the shoot position
+            m_missileGameObject = m_shootPosition != null
+                ? Instantiate(m_missilePrefab, m_shootPosition.position, m_shootPosition.rotation)
+                : Instantiate(m_missilePrefab);
+
+            // Ensure the missile has a NetworkIdentity component
+            if (m_missileGameObject.GetComponent<NetworkIdentity>() == null)
+            {
+                Debug.LogError("Missile prefab is missing NetworkIdentity. Please add it.", m_missileGameObject);
+                return;
+            }
+
+            // Give the missile a unique name using the player's NetworkIdentity netId
+            m_missileGameObject.name = $"{m_missilePrefab.name} [connId={GetComponent<NetworkIdentity>().netId}]";
+
+            // Get the Missile component and assign the player as the owner
             m_missile = m_missileGameObject.GetComponent<Missile>();
             m_missile.PlayerOwner = this;
-        }
 
+            // Spawn the missile on the server and assign the connection of the player as the owner
+            NetworkServer.Spawn(m_missileGameObject, connectionToClient);
+        }
     }
 }
